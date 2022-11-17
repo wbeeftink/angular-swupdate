b844ba654abb27b9a831e15ffdd37232175b9c47
Wouter Beeftink <wouter.beeftink@gmail.com>
2022-11-17 14:48:57 +0100

    Tweaks to UI

--- src/app/baz/baz-routing.module.ts
+++ src/app/baz/baz-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BazComponent } from "./baz.component";
 
-const routes: Routes = [
-  { path: "", pathMatch: "full", component: BazComponent },
-];
+const routes: Routes = [{ path: "", component: BazComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
